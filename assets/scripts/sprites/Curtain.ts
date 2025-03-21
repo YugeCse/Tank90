ed820fb0a57246b7dbb5bdcaefdd7305e1b88817
Hwang <huangzhongyu@sph.com.sg>
2025-03-21 13:30:02 +0800

    #优化幕布控制类

--- assets/scripts/sprites/Curtain.ts
+++ assets/scripts/sprites/Curtain.ts
@@ -1,23 +1,29 @@
-import { _decorator, Animation, Component, Node } from "cc";
+import { _decorator, Animation, CCInteger, Component, Node } from "cc";
 const { ccclass, property } = _decorator;
 
 /** 关卡幕布控制类 */
 @ccclass("Curtain")
 export class Curtain extends Component {
+	@property({ type: CCInteger, displayName: "关卡" })
+	public stage: number = 1; //关卡
+
 	start() {
 		this.scheduleOnce(this.openCurtains, 2); //等待2s后打开幕布
 	}
 
 	update(deltaTime: number) {}
 
-    /** 打开幕布 */
+	/** 设置关卡展示 */
+	public setStage(stage: number) {
+		this.stage = stage;
+	}
+
+	/** 打开幕布 */
 	public openCurtains() {
 		var topAnimation = this.node
-			.getChildByName("Mask")
 			.getChildByName("TopCurtain")
 			.getComponent(Animation);
 		var bottomAnimation = this.node
-			.getChildByName("Mask")
 			.getChildByName("BottomCurtain")
 			.getComponent(Animation);
 		bottomAnimation.on(
@@ -28,10 +34,10 @@ export class Curtain extends Component {
 		);
 		topAnimation.play("curtain_top_open");
 		bottomAnimation.play("curtain_bottom_open");
-		this.node.getChildByName("Mask").getChildByName("TitleContainer").destroy();
+		this.node.getChildByName("TitleContainer").destroy();
 	}
 
-    /** 当幕布打开完成事件 */
+	/** 当幕布打开完成事件 */
 	public onCurtainOpenFinished() {
 		this.node.destroy(); //销毁这个节点
 	}
